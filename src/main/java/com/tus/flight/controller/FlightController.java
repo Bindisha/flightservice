ce998f684dc954bbab9571e79b3743e8dc9059dc
Bindisha <80262203+Bindisha@users.noreply.github.com>
2024-11-28 17:39:04 +0000

    Update FlightController.java

--- src/main/java/com/tus/flight/controller/FlightController.java
+++ src/main/java/com/tus/flight/controller/FlightController.java
@@ -4,21 +4,29 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.tus.flight.model.Flight;
 import com.tus.flight.repo.FlightRepository;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 @RestController
 public class FlightController {
 
-	@Autowired
-	FlightRepository repo;
+	private Logger logger = LoggerFactory.getLogger(FlightController.class);
 
-	@GetMapping("/flights")
-	public List<Flight> getFlights() {
-		return repo.findAll();
+    @Autowired
+    private FlightRepository repo;
 
-	}
+    @GetMapping("/flights")
+    public List<Flight> getFlights(@RequestParam(required = false, defaultValue = "No Request Provided") String request) {
+        logger.info("Received request: " + request);
+        List<Flight> flights = repo.findAll();
+        logger.info("Processed request successfully");
+        return flights;
+    }
 
 }
